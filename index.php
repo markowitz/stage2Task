b7c024b9a28044c86d128fa1155510c6931f9285
markowitz <icnwakanma@gmail.com>
2018-04-26 00:01:06 +0100

    added index.php page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        body {
+            background: lightblue url("undraw_virtual_reality_y5ig.png") no-repeat fixed center; 
+        }
+
+        .container {
+            position: relative;
+            float: right;
+            padding: 250px 250px 250px 50px;
+        }
+
+        h2 {
+            color: #2F80ED;
+            font-size: 18px;
+        }
+        .time {
+            color: #2F80ED;
+            font-size: 18px;
+        }
+    
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <h2>@MARKOWITZ</h2>
+            <strong class="time"><?php echo date("h:i:sa") ?></strong>
+        </div>
+    </div>
+</body>
+</html>
